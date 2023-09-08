58538fc4190e5b1c4b8763818f68689867bd7a3b
meetk.parmar <meet.parmar@tao.live>
2023-09-08 13:32:19 +0530

    Create an empty project

---
+++ app/src/main/java/com/meet/project/findingfalcone/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.meet.project.findingfalcone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ThemeColor {
+    val Primary = Color(0xFF000000)
+    val Grey50 = Color(0xFF808080)
+    val Grey20 = Color(0xFF333333)
+    val Secondary = Color(0xFFFFFFFF)
+}
---
+++ app/src/main/java/com/meet/project/findingfalcone/ui/theme/Type.kt
@@ -0,0 +1,22 @@
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.meet.project.findingfalcone.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_medium, FontWeight.W500),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
+val ThemeTypography = Typography(
+    defaultFontFamily = Poppins,
+    overline = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        letterSpacing = 0.4.sp
+    )
+)
